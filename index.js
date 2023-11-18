9b7b2d4473f1ec98e8c5888db6c71be5b7efa597
Ahmed Adel <1022054.ahmed@stemegypt.edu.eg>
2023-11-18 15:57:03 +0200

    a!Please enter the commit message for your changes. Lines starting
    git restore <index.js>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("App Development JSON file.json");
+const router = jsonServer.router("App development JSON file.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
